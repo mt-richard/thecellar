d624853bb98225dcf6f5dd9ec7047ea9b02ed7b5
mt-richard <Richard@fesimoltd.com>
2023-07-16 16:31:52 +0200

    All backend is done

--- Admin/edit_product.php
+++ Admin/edit_product.php
@@ -3,16 +3,15 @@ include 'navbar.php';
 ?>
   <?php
        
-       require_once('../models/product.php');
+      //  require_once('../models/product.php');
      
-          
-              $proId = $_GET['edit'];
-              if (isset($_POST['updateProduct'])) {
-                require_once('../models/product.php');
-                $pro = new product();
-                $result = $pro->update($proId, $_POST['category'],$_POST['name'], $_POST['desc'], $_POST['size'], $_POST['price'], $_POST['image']);
-                echo "<script>alert('$result'); window.history.pushState({}, '', 'product.php'); window.location.reload();</script>";
-              }
+      //         $proId = $_GET['edit'];
+      //         if (isset($_POST['updateProduct'])) {
+      //           require_once('../models/product.php');
+      //           $pro = new product();
+      //           $result = $pro->update($proId, $_POST['category'],$_POST['name'], $_POST['desc'], $_POST['size'], $_POST['price'], $_POST['image']);
+      //           echo "<script>alert('$result'); window.history.pushState({}, '', 'product.php'); window.location.reload();</script>";
+      //         }
        ?>
 
   <main id="main" class="main">
@@ -35,69 +34,116 @@ include 'navbar.php';
     <section class="section dashboard">
 
                     
-    <div class="container">
-      <div class="row">
-      <div class="col-md-6 ">
-    
-        <form method="POST" action="">
-                       <?php
-                          require_once('../models/product.php');
-                          $user = new product();
-                          $result = $user->getById($_GET['edit']);
-                          foreach($result as $row){
-                      ?>
-                          
-                          <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Product Name</label>
-                <input type="text" name="name" value="<?php echo $row['p_name']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Product Category</label>
-                <select name="category" id="exampleInputEmail1" class="form-select" aria-label="Default select example">
-                <option label="<?php echo $row['p_category']; ?>" value="<?php echo $row['p_category']; ?>"  ></option>
-                <?php
-      
-                  require_once('../models/category.php');
-                      $cat = new category();
-                      $result = $cat->getAll();
-                      foreach($result as $myrow){
-                         $rowname = $myrow['c_name'];
-                        echo "<option value='$rowname' label='$rowname'></option>";
-                      }
-                  ?>
-                  
-              </select>
+    <?php
+require_once('../models/product.php');
 
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Product Description</label>
-                <input type="text" name="desc" value="<?php echo $row['p_desc']; ?>"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Product Size</label>
-                <input type="text" name="size" value="<?php echo $row['p_size']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Product Price</label>
-                <input type="number" name="price" value="<?php echo $row['p_price']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Product Image</label>
-                <input type="file" name="image" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                 
-              </div>
-              <?php }?>
+if (isset($_POST['updateProduct'])) {
+    $proId = $_GET['edit'];
+    $product = new product();
+    
+    $imageName = $_FILES['image']['name'];
+    $targetDirectory = '../images/product/';
+    $targetPath = $targetDirectory . $imageName;
+
+    // Check if a new image was uploaded
+    if (!empty($imageName)) {
+        $allowedFormats = array('jpg', 'jpeg', 'png', 'gif');
+        $fileExtension = strtolower(pathinfo($imageName, PATHINFO_EXTENSION));
+        
+        if (!in_array($fileExtension, $allowedFormats)) {
+            echo "<script>alert('Invalid file format. Only JPG, JPEG, PNG, and GIF images are allowed.');</script>";
+            exit;
+        }
+
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $targetPath)) {
+            // Image uploaded successfully, update the product with the new image path
+            $imagePath = $targetPath;
+        } else {
+            $errorCode = $_FILES['image']['error'];
+            if ($errorCode === UPLOAD_ERR_OK) {
+                echo "<script>alert('Failed to move the uploaded file.');</script>";
+            } else {
+                echo "<script>alert('File upload failed with error code: " . $errorCode . "');</script>";
+            }
+            exit;
+        }
+    } else {
+        // No new image uploaded, retain the existing image path in the database
+        $imagePath = $_POST['old_image'];
+    }
+
+    $result = $product->update(
+        $proId,
+        $_POST['category'],
+        $_POST['name'],
+        $_POST['desc'],
+        $_POST['size'],
+        $_POST['price'],
+        $imagePath 
+    );
+
+    echo "<script>alert('$result'); window.location.href = 'product.php';</script>";
+}
+?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <form method="POST" action="" enctype="multipart/form-data">
+                <?php
+                require_once('../models/product.php');
+                $user = new product();
+                $result = $user->getById($_GET['edit']);
+                foreach ($result as $row) {
+                ?>
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Name</label>
+                        <input type="text" name="name" value="<?php echo $row['p_name']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Category</label>
+                        <select name="category" id="exampleInputEmail1" class="form-select" aria-label="Default select example">
+                            <option label="<?php echo $row['p_category']; ?>" value="<?php echo $row['p_category']; ?>"></option>
+                            <?php
+                            require_once('../models/category.php');
+                            $cat = new category();
+                            $result = $cat->getAll();
+                            foreach ($result as $myrow) {
+                                $rowname = $myrow['c_name'];
+                                echo "<option value='$rowname' label='$rowname'></option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Description</label>
+                        <input type="text" name="desc" value="<?php echo $row['p_desc']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Size</label>
+                        <input type="text" name="size" value="<?php echo $row['p_size']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Price</label>
+                        <input type="number" name="price" value="<?php echo $row['p_price']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Image</label>
+                        <input type="file" name="image" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <!-- Hidden input to store the current image path -->
+                        <input type="hidden" name="old_image" value="<?php echo $row['p_img']; ?>">
+                    </div>
+                <?php } ?>
 
                 <input type="submit" name="updateProduct" value="Update" class="btn btn-primary">
-               
-                </form>
-            </div>
-      </div>
+            </form>
+        </div>
+    </div>
 </div>
+
     </section>
 
   </main>
--- db/thecellar.sql
+++ db/thecellar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2023 at 03:49 PM
+-- Generation Time: Jul 16, 2023 at 03:18 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -41,8 +41,10 @@ CREATE TABLE `account` (
 
 INSERT INTO `account` (`a_id`, `a_email`, `a_username`, `a_password`, `a_created`) VALUES
 (1, 'admin@gmail.com', 'admin', 'admin', '2023-07-13 11:59:00'),
-(2, 'mbanzatrichard@gmail.com', 'honofx', '123', '0000-00-00 00:00:00'),
-(4, 'honogeek@gmail.com', 'honore', '123', '2023-07-13 13:59:08');
+(2, 'mbanzatrichard@gmail.com', 'honore', 'honore', '0000-00-00 00:00:00'),
+(4, 'honogeek@gmail.com', 'honore', '123', '2023-07-13 13:59:08'),
+(8, 'serges@gmail.com', 'cyugu_jac', '123', '2023-07-15 17:24:27'),
+(9, 'serges@gmail.com', 'updated', 'updated', '2023-07-15 17:24:35');
 
 -- --------------------------------------------------------
 
@@ -68,7 +70,11 @@ CREATE TABLE `booked` (
 INSERT INTO `booked` (`b_id`, `b_name`, `b_email`, `b_phone`, `b_time`, `b_checkin`, `b_guest`, `b_space`) VALUES
 (1, 'RIchard MBANZA', 'mbanzatrichard@gmail.com', '0787654212', '12:00', '12/04/2023', '4', 'Large'),
 (6, 'honogeek@gmail.com', 'MUGWANEZA', '0792447050', '22:55', '2023-07-20', '5', 'Balcony'),
-(7, 'rukundojacksn@gmail.com', 'HABIMANA', '0792447050', '22:00', '2023-07-05', '3', 'Garden');
+(7, 'rukundojacksn@gmail.com', 'HABIMANA', '0792447050', '22:00', '2023-07-05', '3', 'Garden'),
+(8, 'pazzo@gmail.com', 'Remy Pazzo', '0792447050', '12:40', '2023-07-25', '10', 'Balcony'),
+(9, 'demo@gmail.com', 'Jonassi Africa', '078765533', '13:40', '2023-07-19', '4', 'Coffee Terrace'),
+(11, 'MUGWANEZA', 'mbanzatrichard@gmail.com', '0792447050', '', '2023-07-19', '5', 'Garden'),
+(12, 'honogeek@gmail.com', 'HOME TEst', '0792447050', '13:50', '2023-07-19', '6', 'Coffe Terrace');
 
 -- --------------------------------------------------------
 
@@ -87,7 +93,8 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`c_id`, `c_name`) VALUES
 (1, 'Food'),
-(38, 'MUGWANEZA');
+(38, 'Soft Drinks55'),
+(39, 'Drinks');
 
 -- --------------------------------------------------------
 
@@ -109,7 +116,8 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`co_id`, `co_name`, `co_email`, `co_phone`, `co_message`, `co_created`) VALUES
-(1, 'Honore', 'honofx@gmail.com', '078777777', 'The service is very good', '2023-07-13 16:05:17');
+(1, 'Honore', 'honofx@gmail.com', '078777777', 'The service is very good', '2023-07-13 16:05:17'),
+(2, 'Mugisha', 'mugisha@gmail.com', '0878765', 'Its working as well', '2023-07-15 21:40:27');
 
 -- --------------------------------------------------------
 
@@ -134,7 +142,11 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`p_id`, `p_category`, `p_name`, `p_desc`, `p_size`, `p_price`, `p_img`) VALUES
 (13, 'Drinks', 'Soda', 'Orange', 'Large', 1000, '../images/product/agashyaaa-1-150x150.jpg'),
 (14, 'Food', 'Pizza', 'Vegitables', 'Large', 3000, '../images/product/Burger-150x150.jpg'),
-(15, 'Food', 'Chicken Wings', '4 legs', '4 legs', 4000, '../images/product/fried-chicken-ge0ca49600_640.jpg');
+(15, 'Food', 'Chicken Wings', '4 legs', '4 legs', 4000, '../images/product/fried-chicken-ge0ca49600_640.jpg'),
+(16, 'Food', 'swing', 'Shabuke Volt 43', 'Large', 250, '../images/product/brune-150x150.jpg'),
+(18, 'Drinks', 'Beer', 'Primus', 'Large', 1000, 'Invalid file format. Only JPG, JPEG, PNG, and GIF '),
+(20, 'Food', 'test', 'test', 'Large', 12000, '../images/product/Brochette-300x300.jpg'),
+(23, 'Food', 'Chicken', 'Chicken boiled', '3legs', 2500, '../images/product/fried-chicken-ge0ca49600_640.jpg');
 
 -- --------------------------------------------------------
 
@@ -145,7 +157,7 @@ INSERT INTO `product` (`p_id`, `p_category`, `p_name`, `p_desc`, `p_size`, `p_pr
 CREATE TABLE `subscriber` (
   `s_id` int(11) NOT NULL,
   `s_email` varchar(50) NOT NULL,
-  `s_created` datetime NOT NULL
+  `s_created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -153,8 +165,10 @@ CREATE TABLE `subscriber` (
 --
 
 INSERT INTO `subscriber` (`s_id`, `s_email`, `s_created`) VALUES
-(1, 'mbanzatrichard@gmail.com', '2023-07-13 18:17:38'),
-(2, 'honofox@gmail.com', '2023-07-13 18:17:38');
+(1, 'mbanzatrichard@gmail.com', '2023-07-13 16:17:38'),
+(2, 'honofox@gmail.com', '2023-07-13 16:17:38'),
+(5, 'serges@gmail.com', '0000-00-00 00:00:00'),
+(6, 'kaka@gmail.com', '2023-07-15 21:42:09');
 
 --
 -- Indexes for dumped tables
@@ -179,6 +193,12 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`c_id`);
 
 --
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`co_id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
@@ -198,31 +218,37 @@ ALTER TABLE `subscriber`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `booked`
 --
 ALTER TABLE `booked`
-  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `co_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `subscriber`
 --
 ALTER TABLE `subscriber`
-  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
